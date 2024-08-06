e73a1c1da59f08ae9d90354794ea60f44bebbfad
knford01 <54276167+knford01@users.noreply.github.com>
2024-08-06 14:06:42 -0500

    New With Folders

---
+++ src/types/user.d.ts
@@ -0,0 +1,23 @@
+export type User = {
+    id: string;
+    first_name: string;
+    middle_name: string;
+    last_name: string;
+    email: string;
+    password: string;
+    role: string;
+    role_display: string;
+    active: string;
+};
+
+export type UserRole = {
+    id: number;
+    role: string;
+    display: string;
+};
+
+export interface UserProfile {
+    userId: number;
+    bio: string;
+    avatarUrl: string;
+}
